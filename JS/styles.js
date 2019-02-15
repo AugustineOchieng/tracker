9a3997b501b0f2a8a822bfc5fdcff137e7beb7d9
AugustineOchieng <gusochieng@gmail.com>
2019-02-15 15:21:47 +0300

    add lengths for comparison

--- JS/styles.js
+++ JS/styles.js
@@ -4,4 +4,9 @@ function trackInt(){
   entry001 = parseInt(document.getElementById(triangleValue1).value)
   entyr002 = parseInt(document.getElementById(triangleValue2).value)
   entry003 = parseInt(document.getElementById(triangleValue3).value)
+  //input sides
+  length1 = entry001 + entry002
+  length2 = entry002 + entry003
+  lenght3 = entry003 + entry001
+  //control flow
 }
