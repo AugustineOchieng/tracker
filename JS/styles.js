f52d612979a25f2f07fb39c5b76b3224a557f10a
AugustineOchieng <gusochieng@gmail.com>
2019-02-15 15:14:22 +0300

    create function and variables

---
+++ JS/styles.js
@@ -0,0 +1,7 @@
+//create a function
+function trackInt(){
+  //create variables
+  entry001 = parseInt(document.getElementById(triangleValue1).value)
+  entyr002 = parseInt(document.getElementById(triangleValue2).value)
+  entry003 = parseInt(document.getElementById(triangleValue3).value)
+}
