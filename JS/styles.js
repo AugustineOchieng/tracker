d4f61bc99fc5c42294e1ecf846383dfc01489855
AugustineOchieng <gusochieng@gmail.com>
2019-02-15 18:04:09 +0300

    add README.md

--- JS/styles.js
+++ JS/styles.js
@@ -1,27 +1,27 @@
 //create a function
 function trackInt(){
   //create variables
-  entry001 = parseInt(document.getElementById(triangleValue1).value)
-  entyr002 = parseInt(document.getElementById(triangleValue2).value)
-  entry003 = parseInt(document.getElementById(triangleValue3).value)
+  input001 = parseInt(document.getElementById("triangleValue1").value)
+  input002 = parseInt(document.getElementById("triangleValue2").value)
+  input003 = parseInt(document.getElementById("triangleValue3").value)
   //input sides
-  length1 = entry001 + entry002
-  length2 = entry002 + entry003
-  lenght3 = entry003 + entry001
-  //control flow
-  if(length1 == length2 && length2 == length3 && length3 == length1){
+  length1 = input001 + input002
+  length2 = input002 + input003
+  length3 = input003 + input001
+  //control flow 
+    if(length1 == length2 && length2 == length3 && length3 == length1){
     alert ("equilateral")
     }
     else if(length1 !== length2 && length2 == length3){
     alert ("isocseles")
     }
     else if(length3 !== length2 && length2 == length1){
-      alert ("isocseles")
-      }
+    alert ("isocseles")
+    }
     else if(length1 !== length2 && length2 !== length3 && length3 !== length1){
-      alert ("Scalene")
-      }
+    alert ("Scalene")
+    }
     else if(length1 >= length2 || length2 >= length3 || length3 >= length1){
-      alert("Not a triangle")
-      }
-}
+    alert("Not a triangle")
+    }
+  }
---
+++ README.md
@@ -0,0 +1,28 @@
+# Triangle Tracker
+#### The Triangle Tracker is an application that helps one know whether a triangle is isosceles, equilateral, scalene or NOT a triangle at all.
+Current Version 15/02/2019
+#### By **Augustine Ochieng**
+## Description
+This application is an essential mathematical tool simplifying a major geometrical function in determinig whether a figure is an issoceles, equilateral, scalene or not a triangle simply by inputting the values of the sides a figure has.
+## Setup/Installation Requirements
+* Type in first length
+* Type in second value
+* Type in third value
+* Click on the submit button
+* Reset workspace
+## Known Bugs
+The automatic reset button is yet to be automated to automatically reset workspace
+## Technologies Used
+HTML
+CSS
+Coolors.co
+Google Fonts
+## Support and contact details
+Everyone interested is most welcome to fork this code and modify it for updates.
+In case of any inconvenience experienced reach out to me through my email address: gusochieng@gmail.com
+##Behaviour Driven Development
+
+### License
+*MIT License*
+Copyright (c) 2018 **Augustine Ochieng**
+  
